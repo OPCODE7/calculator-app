68d974afb354ffb199866f9b15daf0047bbbed01
Op Code <suarezdarlin41@gmail.com>
2023-07-13 20:18:25 -0600

    Finish design themes app

--- js/theme.js
+++ js/theme.js
@@ -4,6 +4,9 @@ export default function changeThemes(buttons) {
     const $buttonsThemes = d.querySelectorAll(buttons);
 
     $buttonsThemes.forEach(btn => {
+        
+        
+        
         btn.addEventListener("click", e => {
             const $theme= d.querySelector(`link[data-theme= "theme-style"]`);
             const pathStylesheet= {
@@ -11,6 +14,11 @@ export default function changeThemes(buttons) {
                 "btn-light-theme": "css/light_theme.css",
                 "btn-purple-theme": "css/purple_theme.css"
             }
+            if(e.target.classList.contains("opacity-none")) e.target.classList.remove("opacity-none");
+
+            $buttonsThemes.forEach(button => {
+                if(e.target.id!==button.id) button.classList.add("opacity-none"); 
+            });
             
             $theme.href= pathStylesheet[btn.id];
         });
