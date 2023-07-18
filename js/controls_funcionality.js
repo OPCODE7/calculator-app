afa8db821a922e66d979aa710d7527065ae269ee
Op Code <suarezdarlin41@gmail.com>
2023-07-17 20:50:24 -0600

    Validations for controls funcionality

--- js/controls_funcionality.js
+++ js/controls_funcionality.js
@@ -1,43 +1,65 @@
-export default function controlsFuncionality(inputSelector){
-    const d= document;
-    const $inputControl= d.querySelector(inputSelector)
-    const $inputControls= d.querySelectorAll(".container__calculator__controls__buttons");
-    d.addEventListener("click",e => {
-        if(e.target.matches("#btn-get-value > span")){
-            $inputControl.value+= e.target.textContent;
+export default function controlsFuncionality(inputSelector) {
+    const d = document;
+    const $inputControl = d.querySelector(inputSelector)
+    const $inputControls = d.querySelectorAll(".container__calculator__controls__buttons");
+    d.addEventListener("click", e => {
+        if (e.target.matches("#btn-get-value > span")) {
+            validateSign(e.target.textContent.charCodeAt(0), e.target.textContent);
         }
-        if(e.target.matches(".reset__button > span")){
-            $inputControl.value= "";
+        if (e.target.matches(".reset__button > span")) {
+            $inputControl.value = "";
         }
-        if(e.target.matches(".delete__button > span")){
-            const arrayValue= $inputControl.value.split('');
+        if (e.target.matches(".delete__button > span")) {
+            const arrayValue = $inputControl.value.split('');
             arrayValue.pop();
-            $inputControl.value= arrayValue.join('');
+            $inputControl.value = arrayValue.join('');
         }
-    }); 
+    });
 
-    d.addEventListener("keypress", e => {
-        let ascciCode= e.key.charCodeAt(0); 
+    d.addEventListener("keyup", e => {
+        e.preventDefault();
+        let key = e.key;
+        let ascciCode = key.charCodeAt(0);
         $inputControls.forEach(control => {
-            if(e.key===control.firstElementChild.textContent){
+            if (key === control.firstElementChild.textContent) {
                 control.classList.add("click__btn");
                 setTimeout(() => {
                     control.classList.remove("click__btn");
                 }, 1000);
             }
         });
-        
 
-        if(ascciCode>=43 && ascciCode<=57){
-            $inputControl.value+= e.key;
-        }
-
-        if(ascciCode===42) $inputControl.value+= "x";
-
-        if(e.key==="Backspace"){
-            const arrayValue= $inputControl.value.split('');
+        if (key === "Backspace") {
+            const arrayValue = $inputControl.value.split('');
             arrayValue.pop();
-            $inputControl.value= arrayValue.join('');
+            $inputControl.value = arrayValue.join('');
         }
+
+        validateSign(ascciCode, key);
     });
+
+    function validateSign(ascciCode, key) {
+        if (ascciCode >= 42 && ascciCode <= 47 || ascciCode===120) {
+            if ($inputControl.value.length > 0) {
+                if (key === "*") {
+                    key = "x";
+                }
+                if (key === ",") {
+                    key = ".";
+                }
+                let lastValue = $inputControl.value.at($inputControl.value.length - 1);
+                if (lastValue.charCodeAt(0) <= 47 || lastValue.charCodeAt(0) === 120) {
+                    let sliceValue = $inputControl.value.slice(0, $inputControl.value.length - 1);
+                    $inputControl.value = sliceValue + key;
+
+                } else {
+                    $inputControl.value += key;
+                }
+            } else{
+                $inputControl.value = "0" + key;
+            }
+        } else if (ascciCode >= 48 && ascciCode <= 57) {
+            $inputControl.value += key;
+        }
+    }
 }
